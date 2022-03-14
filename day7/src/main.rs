c7530b6b502b9df190e5e708de769f876ea3d8f0
Sergey Ternovtsov <tob1az@ukr.net>
2022-03-14 22:36:24 +0200

    Add solution for Day 7 Part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,14 +1,30 @@
 mod data;
 
-fn calculate_solution(positions: &[i32]) -> i32 {
+fn progression(n: i32) -> i32 {
+    n * (2 + n - 1) / 2
+}
+
+fn calculate_solution(positions: &[i32]) -> (i32, i32) {
     let min = *positions.iter().min().unwrap();
     let max = *positions.iter().max().unwrap();
-    (min..=max)
-        .map(|center| positions.iter().map(|p| (p - center).abs()).sum::<i32>())
-        .min()
-        .unwrap()
+    (
+        (min..=max)
+            .map(|center| positions.iter().map(|p| (p - center).abs()).sum::<i32>())
+            .min()
+            .unwrap(),
+        (min..=max)
+            .map(|center| {
+                positions
+                    .iter()
+                    .map(|p| progression((p - center).abs()))
+                    .sum::<i32>()
+            })
+            .min()
+            .unwrap(),
+    )
 }
 
 fn main() {
-    println!("Solution {}", calculate_solution(&data::CRAB_POSITIONS));
+    let (cost1, cost2) = calculate_solution(&data::CRAB_POSITIONS);
+    println!("Solution {} {}", cost1, cost2);
 }
