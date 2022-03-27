ed5458a8e906b3c497db8f21b0458254a51ea0f5
Sergey Ternovtsov <tob1az@ukr.net>
2022-03-27 16:49:22 +0300

    Add solution for Day 11 Part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -7,30 +7,29 @@ extern crate lazy_static;
 #[macro_use(iproduct)]
 extern crate itertools;
 
-
 mod data;
 
-fn calculate_solution(octopuses: &ndarray::Array2<u8>) -> u32 {
+fn calculate_solution(octopuses: &ndarray::Array2<u8>) -> (usize, Option<usize>) {
     let mut flash_count = 0;
+    let mut first_sync_flash_step: Option<usize> = None;
 
     let mut energy_levels = octopuses.clone();
     let shape = energy_levels.shape();
     let x_size = shape[0];
     let y_size = shape[1];
-    for _ in 0..100 {
+    let mut step = 0;
+    while first_sync_flash_step.is_none() {
         energy_levels.map_inplace(|level| *level += 1);
         loop {
-            let mut flashed = false;
+            let mut new_flashes = 0;
             for (x, y) in iproduct!(0..x_size, 0..y_size) {
                 if energy_levels[(x, y)] > 9 {
-                    flashed = true;
-                    flash_count += 1;
+                    new_flashes += 1;
                     energy_levels[(x, y)] = 0;
+
                     let start_x = if x > 0 { x - 1 } else { 0 };
                     let start_y = if y > 0 { y - 1 } else { 0 };
-                    for neighbor_coord in
-                        iproduct!(start_x..=x + 1, start_y..=y + 1)
-                    {
+                    for neighbor_coord in iproduct!(start_x..=x + 1, start_y..=y + 1) {
                         if let Some(neighbor) = energy_levels.get_mut(neighbor_coord) {
                             if *neighbor != 0 {
                                 *neighbor += 1;
@@ -39,14 +38,22 @@ fn calculate_solution(octopuses: &ndarray::Array2<u8>) -> u32 {
                     }
                 }
             }
-            if !flashed {
+            if new_flashes > 0 {
+                if step < 100 {
+                    flash_count += new_flashes;
+                }
+            } else {
+                if energy_levels.iter().filter(|level| **level > 0).count() == 0 {
+                    first_sync_flash_step = Some(step + 1);
+                }
                 break;
             }
         }
+        step += 1;
     }
-    flash_count
+    (flash_count, first_sync_flash_step)
 }
 
 fn main() {
-    println!("Solution {}", calculate_solution(&data::OCTOPUSES));
+    println!("Solution {:?}", calculate_solution(&data::OCTOPUSES));
 }
