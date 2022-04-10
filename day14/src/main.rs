a07a7129eabeeb5246103b8d797356406fc98cbb
Sergey Ternovtsov <tob1az@ukr.net>
2022-04-10 10:38:21 +0300

    Refactor Day 14 solution

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -6,7 +6,10 @@ use itertools::Itertools;
 
 use std::collections::HashMap;
 
-fn make_step(polymer: &String, table: &HashMap<&str, &str>) -> String {
+type LookupTable<'a> = HashMap<&'a str, &'a str>;
+type Histogram = HashMap<char, usize>;
+
+fn make_step(polymer: &String, table: &LookupTable) -> String {
     let mut new_polymer = polymer
         .chars()
         .zip(polymer.chars().skip(1))
@@ -18,22 +21,33 @@ fn make_step(polymer: &String, table: &HashMap<&str, &str>) -> String {
             }
         })
         .collect::<String>();
-    new_polymer.push( polymer
-        .chars().last().unwrap());
+    new_polymer.push(polymer.chars().last().unwrap());
     new_polymer
 }
 
-fn calculate_solution(polymer_template: &str, rules: &[(&str, &str)]) -> usize {
-    let lookup_table: HashMap<_, _> = rules.iter().cloned().collect();
-    let mut result = polymer_template.to_owned();
-    for _ in 0..10 {
-        let new_string = make_step(&result, &lookup_table);
+fn grow_polymer(polymer: String, lookup_table: &LookupTable) -> String {
+    let mut result = polymer;
+    for i in 0..10 {
+        let new_string = make_step(&result, lookup_table);
         result = new_string;
+        println!("{} => {}", i, result.len());
     }
-    println!("{} {}", result, result.len());
-    let mut frequencies = result.chars().unique()
-        .map(|tc| result.chars().filter(|c| *c == tc).count())
-        .collect::<Vec<_>>();
+    result
+}
+
+fn make_histogram(polymer: String) -> Histogram {
+    polymer
+        .chars()
+        .unique()
+        .map(|tc| (tc, polymer.chars().filter(|c| *c == tc).count()))
+        .collect::<Histogram>()
+}
+
+fn calculate_solution(polymer_template: &str, rules: &[(&str, &str)]) -> usize {
+    let lookup_table: LookupTable = rules.iter().cloned().collect();
+    let result = grow_polymer(polymer_template.to_string(), &lookup_table);
+    //println!("{} {}", result, result.len());
+    let mut frequencies = make_histogram(result).values().cloned().collect::<Vec<_>>();
     frequencies.sort();
     println!("{:?}", frequencies);
     frequencies.iter().last().unwrap() - frequencies[0]
