0e64e3d97b17f2ba3986fe104eabd0412cc620ee
Sergey Ternovtsov <tob1az@ukr.net>
2022-06-08 22:48:31 +0300

    Add solution for Day 21 Part 2

--- day21/src/dirac_dice.rs
+++ day21/src/dirac_dice.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 pub trait Die {
     fn roll(&mut self) -> usize;
 }
@@ -23,23 +25,27 @@ impl Die for DeterministicDie {
     }
 }
 
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct Score(pub usize);
+
+#[derive(Clone)]
 pub struct Player {
     pub space: usize,
-    pub score: usize,
-    max_score: usize,
+    pub score: Score,
+    max_score: Score,
 }
 
 impl Player {
-    pub fn new(space: usize, max_score: usize) -> Self {
+    pub fn new(space: usize, max_score: Score) -> Self {
         Self {
             space,
-            score: 0,
+            score: Score(0),
             max_score,
         }
     }
 
     pub fn won(&self) -> bool {
-        self.score >= self.max_score
+        self.score.0 >= self.max_score.0
     }
 
     pub fn spin_by(&mut self, spaces: usize) {
@@ -47,7 +53,7 @@ impl Player {
     }
 
     pub fn end_move(&mut self) {
-        self.score += self.space;
+        self.score.0 += self.space;
     }
 }
 
@@ -67,5 +73,104 @@ pub fn play(die: &mut dyn Die, players: &mut [Player; 2]) -> DieRolls {
                 return rolls;
             }
         }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Frequency(pub usize);
+
+fn find_roll_frequencies(die_count: usize, sides: usize) -> Vec<(Score, Frequency)> {
+    (0..die_count)
+        .map(|_| (1..=sides))
+        .multi_cartesian_product()
+        .map(|d| d.iter().sum())
+        .sorted()
+        .map(|s| (s, 1))
+        .coalesce(|(a, m), (b, n)| {
+            if a == b {
+                Ok((a, m + 1))
+            } else {
+                Err(((a, m), (b, n)))
+            }
+        })
+        .map(|(s, f)| (Score(s), Frequency(f)))
+        .collect_vec()
+}
+
+pub fn count_dirac_dice_wins(player0: Player, player1: Player) -> [usize; 2] {
+    let mut player0 = player0;
+    let mut player1 = player1;
+    let mut player0_wins = 0;
+    let mut player1_wins = 0;
+    let universes = 1;
+    count_dirac_dice_wins_in_one_turn(
+        &mut player0,
+        &mut player1,
+        &mut player0_wins,
+        &mut player1_wins,
+        universes,
+    );
+
+    [player0_wins, player1_wins]
+}
+
+fn count_dirac_dice_wins_in_one_turn(
+    current_player: &mut Player,
+    opponent: &mut Player,
+    current_player_wins: &mut usize,
+    opponent_wins: &mut usize,
+    universes: usize,
+) {
+    for (Score(roll), Frequency(die_combinations)) in find_roll_frequencies(3, 3).into_iter() {
+        let mut player = current_player.clone();
+        player.spin_by(roll);
+        player.end_move();
+        let new_universes = die_combinations * universes;
+        if player.won() {
+            *current_player_wins += new_universes;
+        } else {
+            // turn goes to the opponent
+            count_dirac_dice_wins_in_one_turn(
+                opponent,
+                &mut player,
+                opponent_wins,
+                current_player_wins,
+                new_universes,
+            )
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn can_find_roll_frequencies() {
+        assert_eq!(
+            find_roll_frequencies(3, 3),
+            vec![
+                (Score(3), Frequency(1)),
+                (Score(4), Frequency(3)),
+                (Score(5), Frequency(6)),
+                (Score(6), Frequency(7)),
+                (Score(7), Frequency(6)),
+                (Score(8), Frequency(3)),
+                (Score(9), Frequency(1)),
+            ]
+        );
+    }
+
+    #[test]
+    fn player_is_cloneable() {
+        let original = Player {
+            space: 1,
+            score: Score(2),
+            max_score: Score(3),
+        };
+        let clone = original.clone();
+        assert_eq!(original.space, clone.space);
+        assert_eq!(original.score, clone.score);
+        assert_eq!(original.max_score, clone.max_score);
     }
 }
--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,27 +1,38 @@
 mod data;
+extern crate itertools;
+
 mod dirac_dice;
 
-use dirac_dice::{DeterministicDie, Player};
+use dirac_dice::{DeterministicDie, Player, Score};
 
-fn calculate_solution(start_positions: &[usize; 2]) -> usize {
+fn calculate_solution(start_positions: &[usize; 2]) -> (usize, usize) {
     let goal = 1000;
     let mut players = [
-        Player::new(start_positions[0], goal),
-        Player::new(start_positions[1], goal),
+        Player::new(start_positions[0], Score(goal)),
+        Player::new(start_positions[1], Score(goal)),
     ];
     let mut die = DeterministicDie::with_sides(100);
     let rolls = dirac_dice::play(&mut die, &mut players);
     let (n, loser) = players
         .iter()
         .enumerate()
-        .filter(|(_, p)| !p.won())
-        .next()
+        .find(|(_, p)| !p.won())
         .unwrap();
     println!(
         "Loser {n} has score {}, number of rolls {}",
-        loser.score, rolls
+        loser.score.0, rolls
     );
-    loser.score * rolls
+
+    let goal = 21;
+    let max_winning_combinations = dirac_dice::count_dirac_dice_wins(
+        Player::new(start_positions[0], Score(goal)),
+        Player::new(start_positions[1], Score(goal)),
+    )
+    .into_iter()
+    .max()
+    .unwrap();
+
+    (loser.score.0 * rolls, max_winning_combinations)
 }
 
 fn main() {
